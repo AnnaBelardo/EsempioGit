38884ec8138337230acc0204d7236a28deb26d98
ANNA BELARDO <anna.belardo94@gmail.com>
2019-12-18 11:41:24 +0100

    no nome

--- EsempioHelloWorld/src/HelloWorld.java
+++ EsempioHelloWorld/src/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
     }
     
     public void sayBye() {
-        System.out.println("Bye Bye " + nome + nome + nome);
+        System.out.println("Bye Bye " );
     }
     
     public void sayGoodBye() {
